48cb234b5ac57393e4e515ed884ca4924fa57d5c
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2022-12-08 14:41:23 -0800

    Switch to using logger

--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -12,7 +12,7 @@ WALLPAPER_NIGHT="Slumber by Louis Coyle.png"
 DAWN_START=6
 DAY_START=12
 DUSK_START=17
-NIGHT_START=22
+NIGHT_START=20
 
 # Configuration path
 CONFIG_PATH="org.gnome.desktop.background picture-uri-dark"
@@ -20,20 +20,20 @@ CONFIG_PATH="org.gnome.desktop.background picture-uri-dark"
 # Get the current hour of the day
 HOUR=$(date +%H)
 
-# Log file
-exec >>/home/lucien/dev/wallpaper-changer/wallpaper-changer.log
+# Redirect stdout and stderr to logger
+exec 1> >(logger -t wallpaper-changer) 2>&1
 
 # Set the wallpaper based on the time of day
 if [ $HOUR -ge $DAWN_START -a $HOUR -lt $DAY_START ]; then
-  echo "$(date +"%Y/%m/%d %H:%M:%S") Setting wallpaper to '$WALLPAPER_DAWN' because it is $HOUR hours"
+  echo "Setting wallpaper to '$WALLPAPER_DAWN' because it is $HOUR hours"
   gsettings set $CONFIG_PATH "$WALLPAPER_DIRECTORY/$WALLPAPER_DAWN"
 elif [ $HOUR -ge $DAY_START -a $HOUR -lt $DUSK_START ]; then
-  echo "$(date +"%Y/%m/%d %H:%M:%S") Setting wallpaper to '$WALLPAPER_DAY' because it is $HOUR hours"
+  echo "Setting wallpaper to '$WALLPAPER_DAY' because it is $HOUR hours"
   gsettings set $CONFIG_PATH "$WALLPAPER_DIRECTORY/$WALLPAPER_DAY"
 elif [ $HOUR -ge $DUSK_START -a $HOUR -lt $NIGHT_START ]; then
-  echo "$(date +"%Y/%m/%d %H:%M:%S") Setting wallpaper to '$WALLPAPER_DUSK' because it is $HOUR hours"
+  echo "Setting wallpaper to '$WALLPAPER_DUSK' because it is $HOUR hours"
   gsettings set $CONFIG_PATH "$WALLPAPER_DIRECTORY/$WALLPAPER_DUSK"
 else
-  echo "$(date +"%Y/%m/%d %H:%M:%S") Setting wallpaper to '$WALLPAPER_NIGHT' because it is $HOUR hours"
+  echo "Setting wallpaper to '$WALLPAPER_NIGHT' because it is $HOUR hours"
   gsettings set $CONFIG_PATH "$WALLPAPER_DIRECTORY/$WALLPAPER_NIGHT"
 fi
